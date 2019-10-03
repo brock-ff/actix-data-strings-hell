8dfe03c60656e784aa795f54263fb9837b770f6f
Brock Smedley <brock@firstfoundry.co>
2019-10-02 20:01:33 -0700

    make it work
    Rust is fun

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,16 @@ use actix_web::{web, web::Path, App, HttpServer};
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 
-struct MyData {
-    // hashmap containing game_id -> blockchain_url mappings
-    chain_maps: Mutex<HashMap<String, String>>,
-}
-
 /// Use `Data<T>` extractor to access data in handler.
-fn index(data: web::Data<Arc<MyData>>, path: Path<String>) {
+fn index(data: web::Data<Arc<Mutex<HashMap<String, String>>>>, path: Path<String>) {
     let game_id: String = path.into_inner();
-    let chain_maps = data.chain_maps.lock().unwrap();
-    let blockchain_url = chain_maps.get(&game_id);
+    let mut chain_map = data.lock().unwrap();
+    let blockchain_url = chain_map.get(&game_id);
     if blockchain_url.is_some() {
-        println!("Found blockchain_url: {}", blockchain_url.unwrap());
+        println!("blockchain_url: {}", blockchain_url.unwrap());
     } else {
-        println!("No blockchain_url associated. Pulling from DB.");
-        data.chain_maps
-            .lock()
-            .unwrap()
-            .insert(game_id, "http://sample:8545".to_string());
+        println!("No blockchain_url found for this game. Fetching from DB...");
+        chain_map.insert(game_id, "http://sampleurl:8545".to_owned());
     }
 }
 
@@ -27,9 +19,7 @@ fn main() {
     HttpServer::new(|| {
         App::new()
             // Store `MyData` in application storage.
-            .data(Arc::new(MyData {
-                chain_maps: Mutex::new(HashMap::new()),
-            }))
+            .data(Arc::new(Mutex::new(HashMap::<String, String>::new())))
             .service(web::resource("/{id}").route(web::get().to(index)))
     })
     .bind("127.0.0.1:1337")
