1a0d1c70fcc0e1e9c0a41850470bb24e3a1977ae
Brock Smedley <brock@firstfoundry.co>
2019-10-02 20:29:19 -0700

    refactor to use a struct
    for if you ever wanna hold multiple values instead of just one hashmap

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,26 @@ use actix_web::{web, web::Path, App, HttpServer};
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 
-type SyncHashMap = Arc<Mutex<HashMap<String, String>>>;
+struct AppData {
+    map: HashMap<String, String>,
+}
+
+type SyncHashMap = Arc<Mutex<AppData>>;
 type KeyValData = web::Data<SyncHashMap>;
 
 /// Use `Data<T>` extractor to access data in handler.
 fn index(data: KeyValData, path: Path<String>) {
     let game_id: String = path.into_inner();
-    let mut chain_map = data.lock().unwrap();
-    let blockchain_url = chain_map.get(&game_id);
+    let mut app_data = data.lock().unwrap();
+
+    let blockchain_url = app_data.map.get(&game_id);
     if blockchain_url.is_some() {
         println!("blockchain_url: {}", blockchain_url.unwrap());
     } else {
         println!("No blockchain_url found for this game. Fetching from DB...");
-        chain_map.insert(game_id, "http://sampleurl:8545".to_owned());
+        app_data
+            .map
+            .insert(game_id, "http://sampleurl:8545".to_owned());
     }
 }
 
@@ -22,7 +29,9 @@ fn main() {
     HttpServer::new(|| {
         App::new()
             // Store `MyData` in application storage.
-            .data(Arc::new(Mutex::new(HashMap::<String, String>::new())))
+            .data(Arc::new(Mutex::new(AppData {
+                map: HashMap::<String, String>::new(),
+            })))
             .service(web::resource("/{id}").route(web::get().to(index)))
     })
     .bind("127.0.0.1:1337")
